8367feeb03d6e2e1e118f05c217a80cb22f9b319
tadayoshi_otsuka <ohtukayoshi.yoshi@i.softbank.jp>
2019-11-18 05:03:50 +0900

    [add]headのtitleをviewsでそれぞれ可変表示させた

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+
+    def full_title(page_title='')
+        base_title='JunkLife'
+        if page_title.empty?
+            base_title
+        else
+            base_title + ' | ' + page_title
+        end
+    end
+
+    def logged_in?
+        false
+    end
 end
