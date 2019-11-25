d53345ae839407364df232a73ed1705f29968631
tadayoshi_otsuka <ohtukayoshi.yoshi@i.softbank.jp>
2019-11-26 03:04:14 +0900

    [fix]ユーザー登録、ログイン後のリダイレクト先をユーザーページに変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,16 @@ class ApplicationController < ActionController::Base
 
     protected
 
+    #新規登録、ログイン後のリダイレクト先を変更
+    def after_sign_in_path_for(resource)
+      current_user
+    end
+
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up,           keys: [:name])
+      devise_parameter_sanitizer.permit(:sign_up,           keys: [:avatar])
       devise_parameter_sanitizer.permit(:account_update,    keys: [:name])
+      devise_parameter_sanitizer.permit(:account_update,    keys: [:avatar])
     end
 
 
