395356df4fd22257d2c2e652ae405aa0f6a01fe1
tadayoshi_otsuka <ohtukayoshi.yoshi@i.softbank.jp>
2019-11-29 06:26:14 +0900

    [add]ransackによる記事検索機能を実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,11 @@
 class ApplicationController < ActionController::Base
     before_action :configure_permitted_parameters, if: :devise_controller?
+    before_action :set_search
 
+  def set_search
+    @key=Micropost.ransack(params[:q])
+    @search_feeds=@key.result(distinct: true).page(params[:page])
+  end
     protected
 
     #新規登録、ログイン後のリダイレクト先を変更
