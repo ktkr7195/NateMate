3b93dbee63c4ea09f8275ad4bb4721bd56647e9a
tadayoshi_otsuka <ohtukayoshi.yoshi@i.softbank.jp>
2019-12-02 17:49:46 +0900

    [fix]画像のリサイズ条件を修正

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -2,7 +2,8 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
-  process resize_to_limit: [ 300 , 300 ]
+  process resize_to_fill: [500, 500, 'Center']
+
 
   # Choose what kind of storage to use for this uploader:
   storage :file
@@ -30,9 +31,9 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # end
 
   # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process resize_to_fit: [50, 50]
-  # end
+  version :thumb do
+   process resize_to_fill: [300, 300, 'Center']
+   end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
