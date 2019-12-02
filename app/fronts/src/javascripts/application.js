806f541f0dc082e43f2b62bde229e29d40f37fbc
tadayoshi_otsuka <ohtukayoshi.yoshi@i.softbank.jp>
2019-12-02 17:43:46 +0900

    [add fix] 画像プレビュー機能追加、turbolinksでAjaxが機能しない問題を修正

--- app/fronts/src/javascripts/application.js
+++ app/fronts/src/javascripts/application.js
@@ -1,28 +1,30 @@
-$(function() {
-    $('.slider').slick({
-        autoplay:true,
-        autoplaySpeed:4000,
-        speed:1000,
-        accessibility: true,
-        dots:true,
+$(function(){
+  $('.slider').slick({
+    autoplay:true,
+    autoplaySpeed:4000,
+    speed:1000,
+    accessibility: true,
+    dots:true,
 
 
-        });
-        $('.slick-dots li').on('mouseover', function() {
-            $('.slider').slick('goTo', $(this).index());
     });
+    $('.slick-dots li').on('mouseover', function() {
+        $('.slider').slick('goTo', $(this).index());
+});
+});
+$( document ).on('turbolinks:load',function() {
 
-//画像プレビュー
-    function readURL(input) {
-        if (input.files && input.files[0]) {
-          var reader = new FileReader();
-          reader.onload = function (e) {
-            $('#img_prev').attr('src', e.target.result);
-          }
-          reader.readAsDataURL(input.files[0]);
-        }
-      }
-      $("#present_img").change(function(){
-        readURL(this);
-      });
+function readURL(input) {
+  if (input.files && input.files[0]) {
+  var reader = new FileReader();
+  reader.onload = function (e) {
+$('#img_prev').attr('src', e.target.result);
+  }
+  reader.readAsDataURL(input.files[0]);
+  }
+}
+$("#present_img").change(function(){
+  readURL(this);
+});
+
 });
