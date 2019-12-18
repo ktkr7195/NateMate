5470bde295d78a4e6d9946732bed6463adfd1360
tadayoshi_otsuka <ohtukayoshi.yoshi@i.softbank.jp>
2019-12-19 07:44:55 +0900

    [add]capistrano導入

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :rbenv_ruby, '2.5.1'
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, "/var/www/NateMate"
+set :deploy_to, "/var/www/rails/NateMate"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
@@ -87,16 +87,6 @@ namespace :deploy do
         invoke "puma:restart"
       end
     end
-    desc 'Upload database.yml'
-    task :upload do
-      on roles(:app) do |host|
-        if test "[ ! -d #{shared_path}/config ]"
-          execute "mkdir -p #{shared_path}/config"
-        end
-        upload!('config/database.yml', "#{shared_path}/config/database.yml")
-      end
-    end
-before :starting,:upload
 before :starting,:check_revision
 after :finishing,:compile_assets
 after :finishing,:cleanup
