9c6b1a310823248ecc57f0310f89596ec3d8203e
tadayoshi_otsuka <ohtukayoshi.yoshi@i.softbank.jp>
2020-01-06 04:23:23 +0900

    [fix]ローカルのpuma設定を変更

--- config/puma.rb
+++ config/puma.rb
@@ -10,20 +10,16 @@ threads min_threads_count, max_threads_count
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
-#port        ENV.fetch("PORT") { 3000 }
+port        ENV.fetch("PORT") { 3000 }
 
 # Specifies the `environment` that Puma will run in.
 #
-environment ENV.fetch("RAILS_ENV") { "production" }
+environment ENV.fetch("RAILS_ENV") { "development" }
 
 # Specifies the `pidfile` that Puma will use.
 #pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 
 
-bind "unix:///var/www/rails/NateMate/shared/tmp/sockets/puma.sock"
-pidfile "/var/www/rails/NateMate/shared/tmp/pids/puma.pid"
-state_path "/var/www/rails/NateMate/shared/tmp/pids/puma.state"
-stdout_redirect "/var/www/rails/NateMate/shared/log/puma.stdout.log", "/var/www/rails/NateMate/shared/log/puma.stderr.log", true
 
 # Specifies the number of `workers` to boot in clustered mode.
 # Workers are forked web server processes. If using threads and workers together
