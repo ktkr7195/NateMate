54dd3f8dbab5a9ddc0bcc7bf62be8c7e0c35aba9
tadayoshi_otsuka <ohtukayoshi.yoshi@i.softbank.jp>
2020-02-02 22:42:37 +0900

    [add]Rspec記述

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -62,5 +62,17 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  #メソッド省略
   config.include FactoryBot::Syntax::Methods
+
+  #js: true指定したspecのみブラウザを起動してテストする
+  config.before(:each) do |example|
+    if example.metadata[:type] == :system
+      if example.metadata[:js]
+        driven_by :selenium_chrome_headless, screen_size: [1400, 1400]
+      else
+        driven_by :rack_test
+      end
+    end
+  end
 end
