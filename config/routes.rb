a16c1c068242d158f0bfb2c1419df9f3b5e47ef1
tadayoshi_otsuka <ohtukayoshi.yoshi@i.softbank.jp>
2020-02-03 21:17:23 +0900

    [fix]インデントの修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,19 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
   get 'likes/create'
   get 'likes/destroy'
   get 'users/show'
+  get  '/contact' => 'static_pages#contact'
   devise_for :users,:controllers => {
-    :registrations => 'users/registrations',
-    :sessions => 'users/sessions'
+                    :registrations => 'users/registrations',
+                    :sessions => 'users/sessions'
   }
   devise_scope :user do
-    get "sign_in", :to => "users/sessions#new"
-    get "sign_out", :to => "users/sessions#destroy"
+    get "sign_in", :to => 'users/sessions#new'
+    get "sign_out",:to => 'users/sessions#destroy'
   end
   resources :users
   resources :microposts,          only: [:new, :create, :destroy, :show, :index]
   resources :relationships,       only: [:create, :destroy]
-  resources :likes, only: [:create, :destroy]
-
-
-  root 'static_pages#home'
-
-  get  '/contact' => 'static_pages#contact'
+  resources :likes,               only: [:create, :destroy]
 end
