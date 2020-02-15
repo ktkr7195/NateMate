9ae87a026bb97a66860f78e8186e045f292b4349
tadayoshi_otsuka <ohtukayoshi.yoshi@i.softbank.jp>
2020-02-15 12:59:20 +0900

    [fix]煩雑な分岐をヘルパーメソッド作成でまとめた、命名変更

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,13 @@
 module UsersHelper
 
-    def f4f?(user)
+    def follow_exchanged?(user)
         return false unless user.following?(current_user) && current_user.following?(user)
+
+        true
+    end
+
+    def allowed_to_edit?(user)
+        return false unless user == current_user && user.name != 'guest'
 
         true
     end
