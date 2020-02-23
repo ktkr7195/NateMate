3ffac48abd5fb855c0c239a7dda2785faf2390f9
tadayoshi_otsuka <ohtukayoshi.yoshi@i.softbank.jp>
2020-02-24 02:44:25 +0900

    [fix]インデントの修正

--- app/helpers/microposts_helper.rb
+++ app/helpers/microposts_helper.rb
@@ -1,17 +1,4 @@
 module MicropostsHelper
-  # 画像のExifから緯度経度を取得
-  def get_exif(model)
-    require 'exifr/jpeg'
-    @exif = if Rails.env.production?
-              EXIFR::JPEG.new(open(model.picture.url))
-            else
-              EXIFR::JPEG.new(Rails.root.to_s + "/public/#{model.picture.url}")
-            end
-    @lat = @exif.gps_latitude.to_f
-    @lng = @exif.gps_longitude.to_f
-
-  rescue EXIFR::MalformedImage, EXIFR::MalformedJPEG
-  end
 
   def exif_valid?(model)
     return true if model.exif_is_valid == true && model.latitude.present? && model.longitude.present? \
@@ -25,5 +12,21 @@ module MicropostsHelper
                                  && model.latitude != 0.0 && model.longitude != 0.0
 
     false
+  end
+
+
+  private
+  # 画像のExifから緯度経度を取得
+  def get_exif(model)
+    require 'exifr/jpeg'
+    @exif = if Rails.env.production?
+              EXIFR::JPEG.new(open(model.picture.url))
+            else
+              EXIFR::JPEG.new(Rails.root.to_s + "/public/#{model.picture.url}")
+            end
+    @lat = @exif.gps_latitude.to_f
+    @lng = @exif.gps_longitude.to_f
+
+  rescue EXIFR::MalformedImage, EXIFR::MalformedJPEG
   end
 end
